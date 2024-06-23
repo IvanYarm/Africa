8ba0b5fcd6da556d9c142a014806cf6618fb3119
Ivan <ivan.yarmliuk2@gmail.com>
2024-06-23 16:01:57 -0700

    Finised Gallery view.

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct GalleryView: View {
+    //MARK: - PROPERTIES
+    
+
+    let animal: [AnimalModel] = Bundle.main.decode("animals.json")
+    
+    @State private var selectesAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    let heptic = UIImpactFeedbackGenerator(style: .medium)
+    
+    func gridSwitch() {
+        self.gridLayout = Array(repeating: GridItem(.flexible()), count: Int(self.gridColumn))
+    }
+    
+    //MARK: - BODY
     var body: some View {
-        Text("Gallery Veiw")
+        ScrollView(.vertical, showsIndicators: false) {
+            //Image
+            Image(selectesAnimal)
+                .resizable().scaledToFill()
+                .clipShape(.circle)
+                .overlay(Circle().stroke(Color.white, lineWidth: 1))
+            //Slider
+            Slider(value: $gridColumn, in: 2...4, step: 1)
+                .padding(.horizontal)
+                .onChange(of: gridColumn) {
+                    gridSwitch()
+                    heptic.impactOccurred()
+                }
+            
+            //Grid
+            VStack(alignment: .center, spacing: 30, content: {
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10, content: {
+                    ForEach(animal) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.circle)
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture(perform: {
+                                self.selectesAnimal = item.image
+                                heptic.impactOccurred(intensity: 4)
+                            })
+                            
+                    }//:Loop
+                })//:Grid
+                .animation(.easeIn)
+            })//: Vstack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+            .onAppear(perform: {
+                gridSwitch()
+            })
+            
+        }//:Scroll
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+        
     }
 }
 
-#Preview {
-    GalleryView()
-}
+//#Preview {
+//    GalleryView()
+//}
